ffacbd118ad613c4550143082c45bafb87bcd93b
Your Name <you@example.com>
2016-03-16 14:24:33 +0800

    add ImageLoader

---
+++ gen/com/example/universalimageloader/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.universalimageloader;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/example/universalimageloader/UILApplication.java
@@ -0,0 +1,80 @@
+/*******************************************************************************
+ * Copyright 2011-2013 Sergey Tarasevich
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package com.example.universalimageloader;
+
+import java.io.File;
+import java.util.Collection;
+
+import android.annotation.TargetApi;
+import android.app.Application;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.os.StrictMode;
+import android.util.Log;
+
+import com.example.universalimageloader.Constants.Config;
+import com.nostra13.universalimageloader.cache.disc.impl.UnlimitedDiscCache;
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.cache.memory.MemoryCacheAware;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+
+/**
+ * @author Sergey Tarasevich (nostra13[at]gmail[dot]com)
+ */
+public class UILApplication extends Application {
+	@TargetApi(Build.VERSION_CODES.GINGERBREAD)
+	@SuppressWarnings("unused")
+	@Override
+	public void onCreate() {
+		if (Config.DEVELOPER_MODE && Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
+			StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().detectAll().penaltyDialog().build());
+			StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().detectAll().penaltyDeath().build());
+		}
+
+		super.onCreate();
+
+		initImageLoader(getApplicationContext());
+	}
+
+	public static void initImageLoader(Context context) {
+		// This configuration tuning is custom. You can tune every option, you may tune some of them,
+		// or you can create default configuration by
+		//  ImageLoaderConfiguration.createDefault(this);
+		// method.
+		File file = new File(Constants.getSDPath()+"/xujuan");
+		if(!file.exists()){
+			file.mkdirs();
+		}
+		ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(context)
+				 //设置线程的优先级 
+				.threadPriority(Thread.NORM_PRIORITY - 2)
+				 //当同一个Uri获取不同大小的图片，缓存到内存时，只缓存一个。默认会缓存多个不同的大小的相同图片
+				.denyCacheImageMultipleSizesInMemory()
+				 //设置缓存文件的名字 
+				.discCacheFileNameGenerator(new Md5FileNameGenerator())
+				.discCache(new UnlimitedDiscCache(file))
+				 //设置图片下载和显示的工作队列排序 
+				.tasksProcessingOrder(QueueProcessingType.LIFO)
+				.writeDebugLogs() // Remove for release app
+				.build();
+		// Initialize ImageLoader with configuration.
+		ImageLoader.getInstance().init(config);
+		
+	}
+}
